41854a0b7a62c4b44140bd2843d89a215ad575b5
Chengsi <gaochengsi@gmail.com>
2019-03-18 16:45:21 -0700

    add pokedex.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Pokedex from './Pokedex';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="Pokedex">
+        <Pokedex/>;
       </div>
     );
   }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 class Pokecard extends Component {
+    
+
     render(){
         let { name, id, type, base_experience } = this.props;
         let imgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
