363e774faa84fdf84dcb040468862f064199dcb4
annika <annikaslund@gmail.com>
2019-03-18 16:15:15 -0700

    Add Pokecard.js

---
+++ src/Pokecard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Pokecard extends Component {
+    render(){
+        let { name, id, type, base_experience } = this.props;
+        let imgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+        return (
+            <div>
+                <p>{ name }</p>
+                <img src={imgURL} className="pokemon-image" alt="pokemon"/>
+                <p>Type: { type }</p>
+                <p>EXP: { base_experience }</p>
+            </div>
+        );
+    }
+}
+
+export default Pokecard;
